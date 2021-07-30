6182b439efc09099e5fda1ed6361d901243b451f
chinmaykhamkar <khamkarchinmay4@gmail.com>
2021-07-30 22:23:45 +0530

    merge intervals

---
+++ leetcode/mergre_intervals.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> op;
+        vector<int> t;
+        sort(intervals.begin(),intervals.end());
+        int i;
+        int start = INT_MAX;
+        int end = INT_MIN;
+        int tmin,tmax;
+        
+        if(intervals.size() == 1){
+            op.push_back(intervals[0]);
+            return op;
+        }
+        
+        op.push_back(intervals[0]);
+        int j=0;
+        for(i=1;i<intervals.size();i++){
+            if(op[j][1] >= intervals[i][0]){
+                op[j][1] = max(op[j][1],intervals[i][1]);
+            }
+            else{
+                op.push_back(intervals[i]);
+                j++;
+            }
+        }
+        
+        
+        return op;
+    }
+};
