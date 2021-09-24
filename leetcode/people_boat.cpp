2cbd5e5493fcb0102377e1a1ff8c6919168b4768
chinmaykhamkar <khamkarchinmay4@gmail.com>
2021-09-24 21:06:43 +0530

    leetcode fri

---
+++ leetcode/people_boat.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int count = 0;
+        sort(people.begin(),people.end());
+        int i=0,j=people.size()-1;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+                count++;
+            }
+            else{
+                j--;
+                count++;
+            }
+        }
+       
+        return count;
+    }
+};
