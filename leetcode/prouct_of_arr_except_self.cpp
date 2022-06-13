de30e0ad9495bcbd2358d787f8ac905755f55bd6
Chinmay khamkar <khamkarchinmay4@gmail.com>
2022-06-13 00:40:09 -0700

    leetcode sunday

--- leetcode/prouct_of_arr_except_self.cpp
+++ leetcode/prouct_of_arr_except_self.cpp
@@ -31,3 +31,31 @@ public:
         return op;
     }
 };
+
+//optimal approach
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        int i;
+        int n = nums.size();
+        vector<int> op;
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        left[0] = 1;
+        right[n-1] = 1;
+        for(i=1;i<n;i++){
+                left[i] = left[i-1]*nums[i-1];
+        }
+        for(i=n-2;i>=0;i--){           
+            right[i] = right[i+1]*nums[i+1];
+        }
+        
+       
+        for(i=0;i<n;i++){
+            op.push_back(left[i]*right[i]);
+        }
+        return op;
+    }
+};
