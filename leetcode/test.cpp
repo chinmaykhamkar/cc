c5310e80fbb30281df86857206b5091ca577ca21
Chinmay khamkar <khamkarchinmay4@gmail.com>
2022-06-20 19:45:35 -0700

    leetcode monday

--- leetcode/test.cpp
+++ leetcode/test.cpp
@@ -6,35 +6,7 @@ using namespace std;
 int main()
 {
 
-        vector<vector<string>> op;
-        vector<string> temp;
-        unordered_map<string,int> flag;
-        int i,j;
-        for(i=0;i<strs.size();i++){
-            flag[strs[i]] = 0;
-        }
-        print(flag);
-        // for(i=0;i<strs.size();i++){
-        //     string temp1 = strs[i];
-        //     if(flag[strs[i]] == 0){
-        //         for(j=i;j<strs.size();j++){
-        //             string temp2 = strs[j];
-        //             sort(temp1.begin(),temp1.end());
-        //             sort(temp2.begin(),temp2.end());
-        //             if(temp2 == temp2 && strs[i] != strs[j]){
-        //                 temp.push_back(strs[j]);
-        //                 flag[strs[j]] = 1;
-        //               }
-        //             else{
-        //                 temp.push_back(strs[i]);
-        //               }
-        //         }
-        //         cout<<temp<<endl;
-        //         op.push_back(temp);
-        //         temp.clear();
-        //     }    
-            
-        // }
+        cout<<"yres";
         
         return 0;
 }
