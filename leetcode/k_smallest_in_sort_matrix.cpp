9eecd086d519fd76d0bd4edc45262436ba5eb7a6
Chinmay khamkar <khamkarchinmay4@gmail.com>
2022-06-24 00:44:07 -0700

    leetcode thursday

--- leetcode/k_smallest_in_sort_matrix.cpp
+++ leetcode/k_smallest_in_sort_matrix.cpp
@@ -18,3 +18,26 @@ public:
         
     }
 };
+
+//better approach
+
+int low = max(0,k-m);
+    int high = min(n,k);
+    while(low<=high){
+        int cut1 = low+(high-low)/2;
+        int cut2 = k-cut1;
+        int l1 = cut1==0?INT_MIN:arr1[cut1-1];
+        int l2 = cut2==0?INT_MIN:arr2[cut2-1];
+        int r1 = cut1==n?INT_MAX:arr1[cut1];
+        int r2 = cut2==m?INT_MAX:arr2[cut2];
+        
+        if(l1<=r2 && l2<=r1){
+            return max(l1,l2);
+        }
+        else if(l1>r2){
+            high = cut1-1;
+        }
+        else{
+            low = cut1+1;
+        }
+    }
