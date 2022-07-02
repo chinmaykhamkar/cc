5d1ded40b3e7a8d098e3479530661f1b4603d804
Chinmay khamkar <khamkarchinmay4@gmail.com>
2022-07-01 18:36:18 -0700

    leetcode frisday

--- leetcode/pallindrome_LL.cpp
+++ leetcode/pallindrome_LL.cpp
@@ -26,3 +26,51 @@ public:
         return true;
     }
 };
+
+//better sol
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+       
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* temp;
+        ListNode* prev = NULL;
+        while(fast&&fast->next){
+            fast = fast->next->next;
+            temp = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        
+        if(fast){
+            slow=slow->next;
+        }
+        
+        while(slow){
+            if(slow->val!=prev->val){
+                return false;
+            }
+            else{
+                slow = slow->next;
+                prev=prev->next;
+            }
+        }
+       
+        return true;
+       
+    }
+    
+    
+};
