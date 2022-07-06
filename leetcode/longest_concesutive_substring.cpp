8d5c41b510482918c35c803a5042a1a0729ed862
Chinmay khamkar <khamkarchinmay4@gmail.com>
2022-07-05 19:31:29 -0700

    leetcode tuesday

--- leetcode/longest_concesutive_substring.cpp
+++ leetcode/longest_concesutive_substring.cpp
@@ -34,3 +34,36 @@ public:
 //          return longest; 
 //     }
 };
+
+// better understanding
+
+unordered_map<int,int> map;
+         int i,n=nums.size();
+         for(i=0;i<n;i++){
+             map[nums[i]] = 1;
+         }
+                 
+        for(i=0;i<n;i++){
+            if(map.find(nums[i]-1) != map.end()){
+                map[nums[i]] = 0;
+            }
+        }
+                 
+        int ans=0;
+                 
+        for(auto ele:map){
+            if(map[ele.first] == 1){
+                int temp=1;
+                int val=ele.first;
+                while(map.find(val+1) != map.end()){
+                    temp++;
+                    val++;                              
+
+                }
+            
+            ans = max(ans,temp);    
+            }
+            
+        }
+         
+        return ans;
