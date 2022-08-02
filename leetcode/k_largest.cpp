0e80a1b639891da2da60539dbb94065cf76e0294
chinmay <khamkarchinmay4@gmail.com>
2022-08-02 01:05:55 -0700

    leetcode tue

--- leetcode/k_largest.cpp
+++ leetcode/k_largest.cpp
@@ -2,19 +2,30 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         if(k==1) return *max_element(nums.begin(),nums.end());        
-        int max,i;
+        int max,i,n=nums.size();
         sort(nums.begin(),nums.end());
         // nums.erase(unique(nums.begin(),nums.end()),nums.end());
         
-        for(i=0;i<nums.size();i++){
-            if(nums.size()-i == k){
-                max = nums[i];
-                break;
+        
+        
+        
+        
+        return nums[n-k];
+    }
+};
+
+// queue approach
+ priority_queue<int,vector<int>,greater<int>> q;
+        int i;
+        for(i=0;i<k;i++){
+            q.push(nums[i]);
+        }
+        
+        for(i=k;i<nums.size();i++){
+            if(q.top()<nums[i]){
+                q.pop();
+                q.push(nums[i]);
             }
         }
         
-        
-        
-        return max;
-    }
-};
+        return q.top();
