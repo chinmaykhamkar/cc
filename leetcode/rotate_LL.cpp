35900624a1e1d357d041e0136364693bcfc71282
chinmay <khamkarchinmay4@gmail.com>
2022-08-02 01:43:36 -0700

    leetcode tue

--- leetcode/rotate_LL.cpp
+++ leetcode/rotate_LL.cpp
@@ -41,3 +41,55 @@ public:
             return head;
         }
     };
+
+//better approach
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(k==0) return head;
+        if(!head) return NULL;
+        int count=0;
+        ListNode* temp = head;
+        while(temp){
+            count++;
+            temp=temp->next;
+        }        
+        k = k%count;
+        cout<<count;
+        if(k==0) return head;
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        int i;
+        for(i=0;i<k;i++){
+            fast=fast->next;
+        }
+        while(fast->next){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        
+        fast->next=head;
+        head=slow->next;
+        slow->next=NULL;
+        
+        
+        
+        return head;
+        
+        
+    }
+    
+    
+};
