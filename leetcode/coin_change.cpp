e1f10947892732530201ef1eddec936f8f13eb00
chinmay <khamkarchinmay4@gmail.com>
2022-08-11 20:54:32 -0700

    leetcode thu

--- leetcode/coin_change.cpp
+++ leetcode/coin_change.cpp
@@ -22,3 +22,26 @@ public:
         return -1;
     }
 };
+//coin change problem dynamic programming
+ int n = coins.size();
+        int m = amount;
+        vector<vector<int>> dp(n+1,vector<int>(m+1,m+1));
+        
+        int i,j;
+        for(i=0;i<=n;i++){
+            for(j=0;j<=m;j++){
+                if(j==0) dp[i][j] = 0;
+                else if(i==0) dp[i][j] = m+1;
+                else if(j<coins[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
+                }
+                
+            }
+        }
+        
+        return dp[n][m]==m+1? -1:dp[n][m];
+
+
