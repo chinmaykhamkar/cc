15918c839210e3290a9331e530a39e53b1221045
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2022-09-08 20:58:17 -0700

    leetcode thu

---
+++ leetcode/digit_comb_phone.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mp = {
+            '2':'abc',
+            '3':'def',
+            '4':'ghi',
+            '5':'jkl',
+            '6':'mno',
+            '7':'pqrs',
+            '8':'tuv',
+            '9':'wxyz'
+        }
+        store = []
+        def helper(i,s):
+            if len(s) == len(digits):
+                store.append(s)
+                return 
+            for c in mp[digits[i]]:
+                helper(i+1,s+c)
+        
+        if digits:
+            helper(0,"")
+        return store
+        
